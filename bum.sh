83a119690dfd152cd37b8e81f6d9a166780465d2
Daniel Kucera <daniel.kucera@gmail.com>
2022-03-16 22:58:24 +0100

    update bum

--- bum.sh
+++ bum.sh
@@ -1,3 +1,3 @@
-export PATH=$PATH:/storage/Apps/Xilinx/14.7/ISE_DS/ISE/bin/lin/
+export PATH=$PATH:/storage/Apps/Xilinx/14.7/ISE_DS/ISE/bin/lin/:/home/danman/storage/Apps/riscv64-unknown-elf-gcc-8.3.0-2019.08.0-x86_64-linux-ubuntu14/bin/
 
-python3 cansoc.py
+python3 soccorecan.py
